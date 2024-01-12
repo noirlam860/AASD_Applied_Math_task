e8f32f41e25ef741ef18b619a7e108270f30ec36
noirlam860 <144395681+noirlam860@users.noreply.github.com>
2024-01-11 19:17:21 -0500

    Add files via upload

---
+++ HoLaiHenryLAM.py
@@ -0,0 +1,9 @@
+import write_your_name as name_point
+
+def test_hi_my_name_is():
+    assert len(name_point.hi_my_name_is())  > 1 
+    print(name_point.hi_my_name_is())
+    
+
+test_hi_my_name_is()
+
